f39cec88169af7c114f717e2127dfaa2ca20ee9c
mahamadarbele111-hue <mahamadarbele111@gmail.com>
2025-08-21 03:25:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+});
 const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 const fs = require("fs");
 
